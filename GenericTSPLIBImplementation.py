d852d9d9abf5c95f7889b20e4a163db919726b0e
sjrheeders <stefan@synthesis.co.za>
2023-02-19 23:07:49 +0200

    ORTOOLS Implementation

--- GenericTSPLIBImplementation.py
+++ GenericTSPLIBImplementation.py
@@ -54,11 +54,64 @@ def create_data_model(distance_matrix):
     data['depot'] = int(distance_matrix[0][0])
     return data
 
+def distance_callback(from_index, to_index):
+    """Returns the distance between the two nodes."""
+    # Convert from routing variable Index to distance matrix NodeIndex.
+    from_node = manager.IndexToNode(from_index)
+    to_node = manager.IndexToNode(to_index)
+    return data['distance_matrix'][from_node][to_node]
+
+def print_solution(manager, routing, solution):
+    """Prints solution on console."""
+    print('Objective: {} miles'.format(solution.ObjectiveValue()))
+    index = routing.Start(0)
+    plan_output = 'Route for vehicle 0:\n'
+    route_distance = 0
+    while not routing.IsEnd(index):
+        plan_output += ' {} ->'.format(manager.IndexToNode(index))
+        previous_index = index
+        index = solution.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+    plan_output += ' {}\n'.format(manager.IndexToNode(index))
+    plan_output += 'Route distance: {}miles\n'.format(route_distance)
+    print(plan_output)
+
 # Instantiate the data problem
 data = create_data_model(distance_matrix)
 # Create the routing index manager
 manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']), data['num_vehicles'], data['depot'])
 # Create Routing Model
 routing = pywrapcp.RoutingModel(manager)
+# Create the callback and register it with the solver 
+transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+# Set the cost of travel
+routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+# Set the search parameters and a heuristic method for finding the first solution
+search_parameters = pywrapcp.DefaultRoutingSearchParameters()
 
-print(routing)
+# DEFAULT
+# search_parameters.first_solution_strategy = (
+#     routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+#CHRISTOFIDES
+search_parameters.first_solution_strategy = (
+    routing_enums_pb2.FirstSolutionStrategy.CHRISTOFIDES)
+
+#AUTOMATIC SELECTION
+search_parameters.first_solution_strategy = (
+    routing_enums_pb2.FirstSolutionStrategy.AUTOMATIC)
+
+#Savings algorithm (Clarke & Wright)
+search_parameters.first_solution_strategy = (
+    routing_enums_pb2.FirstSolutionStrategy.SAVINGS)
+
+#Sweep algorithm (Wren & Holliday)
+search_parameters.first_solution_strategy = (
+    routing_enums_pb2.FirstSolutionStrategy.SWEEP)
+
+# Solve the instance with search parameters and print solution to console
+solution = routing.SolveWithParameters(search_parameters)
+if solution:
+    print_solution(manager, routing, solution)
+else:
+    print("nothing")
