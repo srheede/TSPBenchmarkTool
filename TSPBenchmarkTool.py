85394738c3dea4a8c28682338be4eb261a829c3e
sjrheeders <stefan@synthesis.co.za>
2023-02-27 15:48:48 +0200

    Add Memory and Runtime Tracking

--- TSPBenchmarkTool.py
+++ TSPBenchmarkTool.py
@@ -1,4 +1,6 @@
 import os
+import time
+import tracemalloc
 import tsplib95
 
 from python_tsp.distances import tsplib_distance_matrix
@@ -9,13 +11,17 @@ from python_tsp.heuristics import solve_tsp_local_search
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
+from randomized_tsp.tsp import tsp
+
 # tsp instances dimensions (0 for undefined)
-min_instance_dimension = 0
-max_instance_dimension = 15
-max_instance_quantity = 1
+min_instance_dimension = 500
+max_instance_dimension = 10000
+max_instance_quantity = 10
+
+print_results_for_each_instance = False
 
 # algorithms to run
-python_tsp_dynamic_programming = True
+python_tsp_dynamic_programming = False
 python_tsp_simulated_annealing = True
 python_tsp_local_search = True
 python_tsp_dynamic_programming_local_search = True
@@ -28,6 +34,10 @@ ortools_christofides = True
 ortools_automatic_strategy = True
 ortools_clarke_and_wright = True
 ortools_wren_and_holliday = True
+
+randomized_tsp_genetic_algorithm = True
+randomized_tsp_ant_colony = True
+
 
 path = 'datasets/tsp/'
 extension = '.tsp'
@@ -87,40 +97,54 @@ for tspinstance in tspinstances:
         elif min_instance_dimension < tspinstance.dimension and max_instance_dimension == 0:
             problems.append(tspinstance)
 
+local_time_total = 0
+local_memory_total = 0
+local_distance_total = 0
+simulated_time_total = 0
+simulated_memory_total = 0
+simulated_distance_total = 0
+dynamic_time_total = 0
+dynamic_memory_total = 0
+dynamic_distance_total = 0
+simulated_local_time_total = 0
+simulated_local_memory_total = 0
+simulated_local_distance_total = 0
+dynamic_local_time_total = 0
+dynamic_local_memory_total = 0
+dynamic_local_distance_total = 0
+simulated_local_p3_time_total = 0
+simulated_local_p3_memory_total = 0
+simulated_local_p3_distance_total = 0
+dynamic_local_p3_time_total = 0
+dynamic_local_p3_memory_total = 0
+dynamic_local_p3_distance_total = 0
+christofides_time_total = 0
+christofides_memory_total = 0
+christofides_distance_total = 0
+clarke_time_total = 0
+clarke_memory_total = 0
+clarke_distance_total = 0
+wen_time_total = 0
+wen_memory_total = 0
+wen_distance_total = 0
+ortools_time_total = 0
+ortools_memory_total = 0
+ortools_distance_total = 0
+cheapest_time_total = 0
+cheapest_memory_total = 0
+cheapest_distance_total = 0
+ant_time_total = 0
+ant_memory_total = 0
+ant_distance_total = 0
+genetic_time_total = 0
+genetic_memory_total = 0
+genetic_distance_total = 0
 
 for problem in problems:
-    print('Results for: ' + problem.name)
-    print()
+    if print_results_for_each_instance == True:
+        print('\nResults for: ' + problem.name + '\n')
 
     distance_matrix = tsplib_distance_matrix(path + problem.name + extension)
-
-    if python_tsp_dynamic_programming == True:
-        permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
-        print('Route distance for python_tsp_dynamic_programming: ' + str(distance))
-
-    if python_tsp_simulated_annealing == True:
-        permutation2, distance2 = solve_tsp_simulated_annealing(distance_matrix)
-        print('Route distance for python_tsp_simulated_annealing: ' + str(distance2))
-
-    if python_tsp_local_search == True:
-        permutation3, distance3 = solve_tsp_local_search(distance_matrix)
-        print('Route distance for python_tsp_local_search: ' + str(distance3))
-
-    if python_tsp_dynamic_programming_local_search == True:
-        permutation4, distance4 = solve_tsp_local_search(distance_matrix, x0=permutation)
-        print('Route distance for python_tsp_dynamic_programming_local_search: ' + str(distance4))
-
-    if python_tsp_simulated_annealing_local_search == True:
-        permutation5, distance5 = solve_tsp_local_search(distance_matrix, x0=permutation2)
-        print('Route distance for python_tsp_simulated_annealing_local_search: ' + str(distance5))
-
-    if python_tsp_dynamic_programming_local_search_ps3 == True:
-        permutation6, distance6 = solve_tsp_local_search(distance_matrix, x0=permutation, perturbation_scheme="ps3")
-        print('Route distance for python_tsp_dynamic_programming_local_search_ps3: ' + str(distance6))
-
-    if python_tsp_simulated_annealing_local_search_ps3 == True:
-        permutation7, distance7 = solve_tsp_local_search(distance_matrix, x0=permutation2, perturbation_scheme="ps3")
-        print('Route distance for python_tsp_simulated_annealing_local_search_ps3: ' + str(distance7))
 
     data = create_data_model(distance_matrix)
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']), data['num_vehicles'], data['depot'])
@@ -129,17 +153,259 @@ for problem in problems:
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
 
-    if ortools_path_cheapest_arc == True:
-        print('Route distance for ortools_path_cheapest_arc: ' + str(run_algorithm('ortools_path_cheapest_arc')))
+    tsp_obj = tsp(distance_matrix)
+
+    if python_tsp_local_search == True:
+        tracemalloc.start()
+        start = time.time()
+        permutation3, distance3 = solve_tsp_local_search(distance_matrix)
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        local_time_total += end - start
+        local_memory_total += memory
+        local_distance_total += distance3
+        if print_results_for_each_instance == True:
+            print('Local Search:\t\t' + str(distance3) + '\t' + str(round(end - start, 4)) + ' Sec\t' + str(round(memory, 4)) + ' MB')
+
+    if python_tsp_simulated_annealing == True:
+        tracemalloc.start()
+        start = time.time()
+        permutation2, distance2 = solve_tsp_simulated_annealing(distance_matrix)
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        simulated_time = end - start
+        simulated_memory = memory
+        simulated_time_total += end - start
+        simulated_memory_total += memory
+        simulated_distance_total += distance2
+        if print_results_for_each_instance == True:
+            print('Simulated Annealing:\t' + str(distance2) + '\t' + str(round(end - start, 4)) + ' Sec\t' + str(round(memory, 4)) + ' MB')
+
+    if python_tsp_dynamic_programming == True:
+        tracemalloc.start()
+        start = time.time()
+        permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        dynamic_time = end - start
+        dynamic_memory = memory
+        dynamic_time_total += end - start
+        dynamic_memory_total += memory
+        dynamic_distance_total += distance
+        if print_results_for_each_instance == True:
+            print('Dynamic Programming:\t' + str(distance) + '\t' + str(round(end - start, 4)) + ' Sec\t' + str(round(memory, 4)) + ' MB')
+
+    if print_results_for_each_instance == True:
+        print()
+
+    if python_tsp_simulated_annealing == True and python_tsp_simulated_annealing_local_search == True:
+        tracemalloc.start()
+        start = time.time()
+        permutation5, distance5 = solve_tsp_local_search(distance_matrix, x0=permutation2)
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        simulated_local_time_total += end - start + simulated_time
+        simulated_local_memory_total += memory + simulated_memory
+        simulated_local_distance_total += distance5
+        if print_results_for_each_instance == True:
+            print('Simulated Annealing then Local Search:\t\t\t' + str(distance5) + '\t' + str(round(end - start + simulated_time, 4)) + ' Sec\t' + str(round(memory + simulated_memory, 4)) + ' MB')
+
+    if python_tsp_simulated_annealing == True and python_tsp_simulated_annealing_local_search_ps3 == True:
+        tracemalloc.start()
+        start = time.time()
+        permutation7, distance7 = solve_tsp_local_search(distance_matrix, x0=permutation2, perturbation_scheme="ps3")
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        simulated_local_p3_time_total += end - start + simulated_time
+        simulated_local_p3_memory_total += memory + simulated_memory
+        simulated_local_p3_distance_total += distance7
+        if print_results_for_each_instance == True:
+            print('Simulated Annealing then Local Search ps3 Variant:\t' + str(distance7) + '\t' + str(round(end - start + simulated_time, 4)) + ' Sec\t' + str(round(memory + simulated_memory, 4)) + ' MB')
+
+    if print_results_for_each_instance == True:
+        print()
+
+    if python_tsp_dynamic_programming == True and python_tsp_dynamic_programming_local_search == True:
+        tracemalloc.start()
+        start = time.time()
+        permutation4, distance4 = solve_tsp_local_search(distance_matrix, x0=permutation)
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        dynamic_local_time_total += end - start + dynamic_time
+        dynamic_local_memory_total += memory + dynamic_memory
+        dynamic_local_distance_total += distance4
+        if print_results_for_each_instance == True:
+            print('Dynamic Programming then Local Search:\t\t\t' + str(distance4) + '\t' + str(round(end - start + dynamic_time, 4)) + ' Sec\t' + str(round(memory + dynamic_memory, 4)) + ' MB')
+
+    if python_tsp_dynamic_programming == True and python_tsp_dynamic_programming_local_search_ps3 == True:
+        tracemalloc.start()
+        start = time.time()
+        permutation6, distance6 = solve_tsp_local_search(distance_matrix, x0=permutation, perturbation_scheme="ps3")
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        dynamic_local_p3_time_total += end - start + dynamic_time
+        dynamic_local_p3_memory_total += memory + dynamic_memory
+        dynamic_local_p3_distance_total += distance6
+        if print_results_for_each_instance == True:
+            print('Dynamic Programming then Local Search ps3 Variant:\t' + str(distance6) + '\t' + str(round(end - start + dynamic_time, 4)) + ' Sec\t' + str(round(memory + dynamic_memory, 4)) + ' MB')
+
+    if print_results_for_each_instance == True:
+        print()
 
     if ortools_christofides == True:
-        print('Route distance for ortools_christofides: ' + str(run_algorithm('ortools_christofides')))
-
-    if ortools_automatic_strategy == True:
-        print('Route distance for ortools_automatic_strategy: ' + str(run_algorithm('ortools_automatic_strategy')))
+        tracemalloc.start()
+        start = time.time()
+        result = run_algorithm('ortools_christofides')
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        christofides_time_total += end - start
+        christofides_memory_total += memory
+        christofides_distance_total += result
+        if print_results_for_each_instance == True:
+            print('Christofides:\t\t\t' + str(result) + '\t' + str(round(end - start, 4)) + ' Sec\t' + str(round(memory, 4)) + ' MB')
 
     if ortools_clarke_and_wright == True:
-        print('Route distance for ortools_clarke_and_wright: ' + str(run_algorithm('ortools_clarke_and_wright')))
+        tracemalloc.start()
+        start = time.time()
+        result = run_algorithm('ortools_clarke_and_wright')
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        clarke_time_total += end - start
+        clarke_memory_total += memory
+        clarke_distance_total += result
+        if print_results_for_each_instance == True:
+            print('Clarke and Wright:\t\t' + str(result) + '\t' + str(round(end - start, 4)) + ' Sec\t' + str(round(memory, 4)) + ' MB')
 
     if ortools_wren_and_holliday == True:
-        print('Route distance for ortools_wren_and_holliday: ' + str(run_algorithm('ortools_wren_and_holliday')))
+        tracemalloc.start()
+        start = time.time()
+        result = run_algorithm('ortools_wren_and_holliday')
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        wen_time_total += end - start
+        wen_memory_total += memory
+        wen_distance_total += result
+        if print_results_for_each_instance == True:
+            print('Wren and Holliday:\t\t' + str(result) + '\t' + str(round(end - start, 4)) + ' Sec\t' + str(round(memory, 4)) + ' MB')
+    
+    if ortools_automatic_strategy == True:
+        tracemalloc.start()
+        start = time.time()
+        result = run_algorithm('ortools_automatic_strategy')
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        ortools_time_total += end - start
+        ortools_memory_total += memory
+        ortools_distance_total += result
+        if print_results_for_each_instance == True:
+            print('ORTools Automatic Strategy:\t' + str(result) + '\t' + str(round(end - start, 4)) + ' Sec\t' + str(round(memory, 4)) + ' MB')
+
+    if ortools_path_cheapest_arc == True:
+        tracemalloc.start()
+        start = time.time()
+        result = run_algorithm('ortools_path_cheapest_arc')
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        cheapest_time_total += end - start
+        cheapest_memory_total += memory
+        cheapest_distance_total += result
+        if print_results_for_each_instance == True:
+            print('ORTools Cheapest Arc Strategy:\t' + str(result) + '\t' + str(round(end - start, 4)) + ' Sec\t' + str(round(memory, 4)) + ' MB')
+    
+    if print_results_for_each_instance == True:
+        print()
+
+    if randomized_tsp_ant_colony == True:
+        tracemalloc.start()
+        start = time.time()
+        tour2, cost2 = tsp_obj.ant_colony()
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        ant_time_total += end - start
+        ant_memory_total += memory
+        ant_distance_total += cost2
+        if print_results_for_each_instance == True:
+            print('Ant Colony:\t\t' + str(cost2) + '\t' + str(round(end - start, 4)) + ' Sec\t' + str(round(memory, 4)) + ' MB')
+
+    if randomized_tsp_genetic_algorithm == True:
+        tracemalloc.start()
+        start = time.time()
+        tour, cost = tsp_obj.genetic_algorithm()
+        end = time.time()
+        memory = tracemalloc.get_traced_memory()[1]/1000000
+        tracemalloc.stop()
+        genetic_time_total += end - start
+        genetic_memory_total += memory
+        genetic_distance_total += cost
+        if print_results_for_each_instance == True:
+            print('Genetic Algorithm:\t' + str(cost) + '\t' + str(round(end - start, 4)) + ' Sec\t' + str(round(memory, 4)) + ' MB')
+
+    if print_results_for_each_instance == True:
+        print()
+
+print('\nResult Totals:\n')
+
+if python_tsp_local_search == True:
+    print('Local Search:\t\t' + str(local_distance_total) + '\t' + str(round(local_time_total, 4)) + ' Sec\t' + str(round(local_memory_total, 4)) + ' MB')
+
+if python_tsp_simulated_annealing == True:
+    print('Simulated Annealing:\t' + str(simulated_distance_total) + '\t' + str(round(simulated_time_total, 4)) + ' Sec\t' + str(round(simulated_memory_total, 4)) + ' MB')
+
+if python_tsp_dynamic_programming == True:
+    print('Dynamic Programming:\t' + str(dynamic_distance_total) + '\t' + str(round(dynamic_time_total, 4)) + ' Sec\t' + str(round(dynamic_memory_total, 4)) + ' MB')
+
+print()
+
+if python_tsp_simulated_annealing == True and python_tsp_simulated_annealing_local_search == True:
+    print('Simulated Annealing then Local Search:\t\t\t' + str(simulated_local_distance_total) + '\t' + str(round(simulated_local_time_total, 4)) + ' Sec\t' + str(round(simulated_local_memory_total, 4)) + ' MB')
+
+if python_tsp_simulated_annealing == True and python_tsp_simulated_annealing_local_search_ps3 == True:
+    print('Simulated Annealing then Local Search ps3 Variant:\t' + str(simulated_local_p3_distance_total) + '\t' + str(round(simulated_local_p3_time_total, 4)) + ' Sec\t' + str(round(simulated_local_p3_memory_total, 4)) + ' MB')
+
+print()
+
+if python_tsp_dynamic_programming == True and python_tsp_dynamic_programming_local_search == True:
+    print('Dynamic Programming then Local Search:\t\t\t' + str(dynamic_local_distance_total) + '\t' + str(round(dynamic_local_time_total, 4)) + ' Sec\t' + str(round(dynamic_local_memory_total, 4)) + ' MB')
+
+if python_tsp_dynamic_programming == True and python_tsp_dynamic_programming_local_search_ps3 == True:
+    print('Dynamic Programming then Local Search ps3 Variant:\t' + str(dynamic_local_p3_distance_total) + '\t' + str(round(dynamic_local_p3_time_total, 4)) + ' Sec\t' + str(round(dynamic_local_p3_memory_total, 4)) + ' MB')
+
+print()
+
+if ortools_christofides == True:
+    print('Christofides:\t\t\t' + str(christofides_distance_total) + '\t' + str(round(christofides_time_total, 4)) + ' Sec\t' + str(round(christofides_memory_total, 4)) + ' MB')
+
+if ortools_clarke_and_wright == True:
+    print('Clarke and Wright:\t\t' + str(clarke_distance_total) + '\t' + str(round(clarke_time_total, 4)) + ' Sec\t' + str(round(clarke_memory_total, 4)) + ' MB')
+
+if ortools_wren_and_holliday == True:
+    print('Wren and Holliday:\t\t' + str(wen_distance_total) + '\t' + str(round(wen_time_total, 4)) + ' Sec\t' + str(round(wen_memory_total, 4)) + ' MB')
+    
+if ortools_automatic_strategy == True:
+    print('ORTools Automatic Strategy:\t' + str(ortools_distance_total) + '\t' + str(round(ortools_time_total, 4)) + ' Sec\t' + str(round(ortools_memory_total, 4)) + ' MB')
+
+if ortools_path_cheapest_arc == True:
+    print('ORTools Cheapest Arc Strategy:\t' + str(cheapest_distance_total) + '\t' + str(round(cheapest_time_total, 4)) + ' Sec\t' + str(round(cheapest_memory_total, 4)) + ' MB')
+    
+print()
+
+if randomized_tsp_ant_colony == True:
+    print('Ant Colony:\t\t' + str(ant_distance_total) + '\t' + str(round(ant_time_total, 4)) + ' Sec\t' + str(round(ant_memory_total, 4)) + ' MB')
+
+if randomized_tsp_genetic_algorithm == True:
+    print('Genetic Algorithm:\t' + str(genetic_distance_total) + '\t' + str(round(genetic_time_total, 4)) + ' Sec\t' + str(round(genetic_memory_total, 4)) + ' MB')
+
+print()
